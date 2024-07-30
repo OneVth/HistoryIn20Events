54df4ceb1e6dcd9c4f86ceb2e7f29a4c5477b57a
OneVth <onebrotravel@gmail.com>
2024-07-30 23:49:19 +0900

    Implement Display Logic
    Update Program.cs
    - Added a loop to iterate through a list of historical events and display their details.
    - Implemented user interaction to display event name, description, and extra description based on key presses.

--- HistoryIn20Events/Program.cs
+++ HistoryIn20Events/Program.cs
@@ -4,7 +4,43 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            while (true)
+            {
+                
+                foreach (HistoricalEvent db in EventsDB.Events)
+                {
+                    Console.Write("This is the year of the historic event: ");
+                    Console.WriteLine(db.Year);
+                    Console.WriteLine();
+
+                    Console.WriteLine("Press any key to show the name of the event.\n");
+                    Console.ReadKey(true);
+                    Console.WriteLine($"Name:\n{db.Name}\n");
+
+                    Console.WriteLine("Press \'d\' to show the description of the event, else to continue.\n");
+
+                    if (Console.ReadKey(true).Key == ConsoleKey.D)
+                    {
+                        Console.WriteLine($"Description:\n{db.Description}\n");
+
+                        if (db.ContainsExtraDescription())
+                        {
+                            Console.WriteLine("Press \'e\' to show the extra description of the event, else to continue.\n");
+                            if (Console.ReadKey(true).Key == ConsoleKey.E)
+                            {
+                                Console.WriteLine($"Extra Description:\n{db.ExtraDescription}\n");
+                            }
+                        }
+                        Console.WriteLine("Press any key to continue.");
+                        Console.ReadKey(true);
+                    }
+                    Console.Clear();
+                }
+
+                Console.WriteLine("Press \'q\' to quit, else to restart.");
+                if (Console.ReadKey(true).Key == ConsoleKey.Q) break;
+            }
+            
         }
     }
 }
